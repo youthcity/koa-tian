c46b55d400bd57e232b8d55a8968b2b1facda73a
tiancity <397731024@qq.com>
2018-02-09 09:50:11 +0800

    refactor(rotuer_loader): use scan to inject rotuer automatically

--- README.md
+++ README.md
@@ -8,6 +8,19 @@
 - 记录日志
 - 错误处理
 
+## NodeJS基础知识
+
+### fs
+
+#### readdir()，readdirSync()
+
+readdir方法用于读取目录，返回一个所包含的文件和子目录的数组。
+
+```javascript
+const dir = path.join(__dirname, './rotuer');
+const files = fs.，readdirSync(dir);
+```
+
 ## 参考资料
 
 - [Nodejs：摆脱黑工坊发展出一款基础企业级框架](https://zhuanlan.zhihu.com/p/33143058)
--- router_loader.js
+++ router_loader.js
@@ -1,7 +1,21 @@
 const router = require('koa-router');
+const fs = require('fs');
+
 const Router = new router();
 
 const User = require('./router/user');
+
+const scan_dir = () => {
+  const url = './router';
+  const dir = fs.readdirSync(url);
+
+  dir.forEach((filename) => {
+    console.log('filename ', filename);
+    const router_model = require(url + '/' + filename);
+    add_routers(router_model);
+  });
+
+};
 
 const add_routers = (router) => {
   Object.keys(router).forEach((key) => {
@@ -12,8 +26,7 @@ const add_routers = (router) => {
 
 
 const set_routers = () => {
-  add_routers(User);
-
+  scan_dir();
   return Router.routes();
 };
 
